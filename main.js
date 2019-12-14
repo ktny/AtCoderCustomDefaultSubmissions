9c1b4233ba0f65b8fb63f46bf93ac3ee1da02528
katsunori.t10@gmail.com <katsunori.t10@gmail.com>
2019-12-14 14:21:22 +0900

    言語を辞書から選択するようにした

--- main.js
+++ main.js
@@ -12,12 +12,31 @@
 $(function() {
     'use strict';
 
+    const lang = 'C++'; // langsの中からよく使用する言語に変更
+    const langs = {
+        'C++': 3003,
+        'C#': 3006,
+        'C': 3002,
+        'Python3': 3023,
+        'PyPy3': 3510,
+        'Ruby': 3024,
+        'Java': 3016,
+        'JavaScript': 3017,
+        'TypeScript': 3521,
+        'PHP': 3524,
+        'Haskell': 3014,
+        'Go': 3013,
+        'Scala': 3025,
+        'Perl': 3020,
+        'Swift': 3503,
+        'Rust': 3504,
+        'Kotlin': 3523
+    };
+
     const params = {
-        'f.Language': 3510,
+        'f.Language': langs[lang],
         'f.Status': 'AC', // WA, TLE, MLE, RE, CE, QLE, OLE, IE, WJ, WR, Judging
         'orderBy': 'source_length', // score, time_consumption, memory_consumption
-        // 'f.Task': '',
-        // 'f.User': '',
     };
 
     const esc = encodeURIComponent;
